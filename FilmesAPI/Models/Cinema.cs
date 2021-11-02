d496a00219d7a266a8d2134703e2eb3c17676e46
Cássio Costa <50739113+cassiocostawk@users.noreply.github.com>
2021-11-02 12:10:35 -0300

    Relacionamento n:n.

--- FilmesAPI/Models/Cinema.cs
+++ FilmesAPI/Models/Cinema.cs
@@ -17,6 +17,8 @@ namespace FilmesAPI.Models
         public string Nome { get; set; }
         public int EnderecoId { get; set; }
         public int GerenteId { get; set; }
+        [JsonIgnore]
+        public virtual List<Sessao> Sessoes { get; set; }
                 
         public virtual Endereco Endereco { get; set; }
         public virtual Gerente Gerente { get; set; }
--- FilmesAPI/Models/Filme.cs
+++ FilmesAPI/Models/Filme.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace FilmesAPI.Models
@@ -20,6 +21,8 @@ namespace FilmesAPI.Models
         [Range(1, 600, ErrorMessage = "A Duração deve ter entre 1 e 600 minutos.")]
         public int Duracao { get; set; }
         public int ClassificacaoEtaria { get; set; }
+        [JsonIgnore]
+        public virtual List<Sessao> Sessoes { get; set; }
 
     }
 }
