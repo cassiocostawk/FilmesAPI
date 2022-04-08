3a5ef01bbbcd82daed4816b0aa4f20912f2dd704
Cássio Costa <50739113+cassiocostawk@users.noreply.github.com>
2022-04-07 23:33:23 -0300

    02. Redefinindo senhas

--- UsuariosApi/Services/LoginService.cs
+++ UsuariosApi/Services/LoginService.cs
@@ -1,5 +1,6 @@
 ﻿using FluentResults;
 using Microsoft.AspNetCore.Identity;
+using System;
 using System.Linq;
 using UsuariosApi.Data.Requests;
 using UsuariosApi.Models;
@@ -27,6 +28,46 @@ namespace UsuariosApi.Services
                 return Result.Ok().WithSuccess(token.Value); 
             }
             return Result.Fail("Login falhou.");
+        }
+
+        public Result SolicitaResetSenhaUsuario(SolicitaResetRequest request)
+        {
+            IdentityUser<int> identityUser = RecuperaUsuarioPorEmail(request.Email);
+
+            if (identityUser != null)
+            {
+                string codigoRecuperacao = _signInManager
+                    .UserManager
+                    .GeneratePasswordResetTokenAsync(identityUser)
+                    .Result;
+
+                return Result.Ok().WithSuccess(codigoRecuperacao);
+            }
+
+            return Result.Fail("Falha ao solicitar redefinição.");
+        }
+
+        public Result ResetaSenhaUsuario(EfetuaResetRequest request)
+        {
+            IdentityUser<int> identityUser = RecuperaUsuarioPorEmail(request.Email);
+
+            IdentityResult resultadoIdentity = _signInManager
+                .UserManager
+                .ResetPasswordAsync(identityUser, request.Token, request.Password)
+                .Result;
+
+            if (resultadoIdentity.Succeeded) return Result.Ok()
+                    .WithSuccess("Senha redefinida com sucesso!");
+
+            return Result.Fail("Falha ao redefinir a senha!");
+        }
+
+        private IdentityUser<int> RecuperaUsuarioPorEmail(string email)
+        {
+            return _signInManager
+                .UserManager
+                .Users
+                .FirstOrDefault(u => u.NormalizedEmail == email.ToUpper());
         }
     }
 }
